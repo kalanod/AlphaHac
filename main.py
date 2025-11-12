26c700bd76e3f352b44351b617c8b847b0d3eef9
andrey <yaidf@ya.ru>
2025-11-12 16:06:00 +0300

    llm_adapter

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from tqdm import tqdm
 
 from modules.db_adapter import DBAdapter
 from modules.embedder import Embedder
+from modules.llm_adapter import LLMAdapter
 from modules.parser import Parser
 from modules.reranker import Reranker
 from modules.writer import Writer
 
 parser = Parser()
 embedder = Embedder()
+llm_adapter = LLMAdapter()
 db_adapter = DBAdapter()
 reranker = Reranker()
 writer = Writer()
@@ -20,6 +22,7 @@ if __name__ == '__main__':
     questions = parser.parse_questions()
     answers = []
     for question in tqdm(questions):
+        question = llm_adapter.normalize(question)
         embedded_question = embedder.embedd_query(question)
         similar_docs = db_adapter.search_in_chroma(embedded_question)
         answer = reranker.rerank_docs(similar_docs, questions)
---
+++ modules/llm_adapter.py
@@ -0,0 +1,3 @@
+class LLMAdapter:
+    def normalize(self, question):
+        pass
