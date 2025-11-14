921037cf0e5d195c4db392667369a94b55cce834
SimhyKim <92271811+SimhyKim@users.noreply.github.com>
2025-11-14 17:35:44 +0300

    Update writer.py

--- modules/writer.py
+++ modules/writer.py
@@ -1,3 +1,13 @@
 class Writer:
+    def __init__(self, filename="alfahack_results.csv"):
+        self.filename = filename
+        
     def write_answers(self, answers):
+        with open(self.filename, mode="w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f, delimiter='\t')  
+            writer.writerow(["q_id", "web_list"])  
+            for idx, answer in enumerate(answers, start=1):
+                web_list_str = str(answer)
+                writer.writerow([idx, web_list_str])
+
         pass
