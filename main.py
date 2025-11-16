dbd4e99af181c094761ef6260e7e53809ad19684
andrey <yaidf@ya.ru>
2025-11-16 22:08:33 +0300

    magicWand

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ writer = Writer()
 
 if __name__ == '__main__':
     documents = parser.parse_train()
+    documents = llm_adapter.magicWand(documents)
     db_adapter.save_to_db(documents)
     questions = parser.parse_questions()
     answers = []
